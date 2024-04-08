117c7ae25f64768bc80ffd7f727906d42c11f8a3
givenng6 <givenng6@gmail.com>
2024-04-08 21:15:44 +0200

    basic module

--- src/user/user.model.ts
+++ src/user/user.model.ts
@@ -1,8 +1,9 @@
 import { Prisma, Role } from '@prisma/client'
 
-export class User  implements Prisma.UserCreateInput {
+export class User implements Prisma.UserCreateInput {
     id: number;
     email: string;
     name: string;
     role: Role;
+    password: string;
 }
---
+++ src/user/user.service.ts
@@ -0,0 +1,7 @@
+import { PrismaService } from "src/services/prisma.service";
+
+export class UserService {
+    constructor(private service: PrismaService){}
+
+    
+}
